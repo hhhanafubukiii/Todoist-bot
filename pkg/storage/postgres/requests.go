f439ea9808884a69f8ed97e1848ad19976a10661
Hana <artemlugovoy201006@gmail.com>
2024-11-10 20:49:41 +0300

    Update requests.go

--- pkg/storage/postgres/requests.go
+++ pkg/storage/postgres/requests.go
@@ -9,11 +9,11 @@ import (
 	"os"
 )
 
-type postgres struct {
+type Postgres struct {
 	db *pgxpool.Pool
 }
 
-func (pg *postgres) SaveAccessToken(ctx context.Context, chatId int64, accessToken string, databaseURL string) error {
+func (pg *Postgres) SaveAccessToken(ctx context.Context, chatId int64, accessToken string, databaseURL string) error {
 	conn := getConn(databaseURL)
 	query := fmt.Sprintf(`INSERT INTO tokens(chat_id, access_token) VALUES (%d, %s)`, chatId, accessToken)
 	_, err := conn.Exec(ctx, query, chatId, accessToken)
@@ -24,7 +24,7 @@ func (pg *postgres) SaveAccessToken(ctx context.Context, chatId int64, accessTok
 	return nil
 }
 
-func (pg *postgres) GetAccessToken(ctx context.Context, chatId int64, databaseURL string) (accessToken string, err error) {
+func (pg *Postgres) GetAccessToken(ctx context.Context, chatId int64, databaseURL string) (accessToken string, err error) {
 	conn := getConn(databaseURL)
 	query := fmt.Sprintf(`SELECT access_token FROM tokens WHERE chat_id = %d`, chatId)
 
