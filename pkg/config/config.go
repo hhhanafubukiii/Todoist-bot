d47b910aadee8f59127d14d32557073e4ab682fb
Hana <artemlugovoy201006@gmail.com>
2024-11-10 21:40:35 +0300

    Delete pkg directory

--- pkg/config/config.go
+++
@@ -1,9 +0,0 @@
-package configs
-
-var Lexiocon = map[string]map[string]string{
-	"response": {
-		"start":              "Привет! Чтобы я мог взаимодействовать с твоим аккаунтом Todoist, тебе необходимо дать мне на это доступ. Для этого переходи по ссылке:\n%s",
-		"already_authorized": "Ты уже авторизировался с помощью своего аккаунта Todoist.",
-		"unknown_command":    "Я не знаю такой команды.",
-	},
-}
